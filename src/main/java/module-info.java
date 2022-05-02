677b21ab50118012db20cb01ca539f1e120f20bc
Andreas Johansson <saerdnajohansson@gmail.com>
2022-05-02 17:51:23 +0200

    initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
-module andjoh {
+module org.dat216 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires projectbackend;
 
     opens org.dat216 to javafx.fxml;
     exports org.dat216;
